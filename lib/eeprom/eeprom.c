03cc19e3a2b54a052d97b2248c989219419a2bf0
Ondros290 <101509970+Ondros290@users.noreply.github.com>
2024-12-11 10:44:52 +0100

    comments in eeprom.c

--- lib/eeprom/eeprom.c
+++ lib/eeprom/eeprom.c
@@ -2,24 +2,25 @@
 
 
 void eeprom_write_byte(uint16_t address, uint8_t data)
-// writes one byte to a specific address in EEPROM
+// write one byte to a specific address in EEPROM
 {
-  twi_start();
+  twi_start(); // send start condition
 
-  twi_write((EEPROM_ADR<<1) | TWI_WRITE);
+  twi_write((EEPROM_ADR<<1) | TWI_WRITE); // shift to the left to make space for the R/W bit and set R/W bit
 
-  twi_write(address>>8);
+  twi_write(address>>8); // send MSB of address
 
-  twi_write(address&0xFF);
+  twi_write(address&0xFF); // send LSB of address
 
-  twi_write(data);
+  twi_write(data); // send byte of data
 
-  twi_stop();
+  twi_stop(); // send stop condition
 }
 
 uint8_t eeprom_read_byte(uint16_t address)
 // read one byte of data from a specific address in EEPROM
 {
+  // write dummy byte to load in the data word address
   twi_start();
 
   twi_write((EEPROM_ADR<<1) | TWI_WRITE);
@@ -28,7 +29,7 @@ uint8_t eeprom_read_byte(uint16_t address)
 
   twi_write(address & 0xFF) == 0;
   
-  twi_start();
+  twi_start(); // send start condition again
 
   twi_write((EEPROM_ADR << 1) | TWI_READ); 
 
